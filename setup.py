3176241c6c1b3bd1e2a1dcae0648f2bbc25bd2be
flrn <flrn.beck@gmail.com>
2020-11-30 22:30:20 +0100

    added all required modules in setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 setup(
     name = 'arxiv-script',
     version = '0.1',
-    py_modules = ['arxiv-script'],
-    install_requires = ['Click',
+    py_modules = ['arxiv-script', 'article', 'retrieve', 'dir_control'],
+    install_requires = ['Click', 
     ],
     entry_points='''
         [console_scripts]
