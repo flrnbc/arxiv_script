1b8b206173386a088b8a62aadc2236a0b857f40a
flrn <flrn.beck@gmail.com>
2020-12-02 21:48:33 +0100

    Basic functionality works: arxiv_script ano checks if ano (arXiv number) exists and then prints the corresponding abstract.
    TODO: - check issue with 0911.---- (use str(input(...))?
    - introduce options
    - give a way to exit properly
    - simplify name of script

--- arxiv_script.py
+++ arxiv_script.py
@@ -3,13 +3,17 @@ import retrieve
 import click
 
 @click.command()
-@click.argument(aX_num)
-def cli(aX_num):
+#@click.option('--ano', default=0))
+@click.argument('ano', type=str, default=0)
+def cli(ano):    
     ''' Script to download arXiv articles. '''
-    while retrieve.check(aX_num) == False: 
-        aX_num = str(input("Please enter a valid arxiv identifier."))
-        aX_num = aX_num.zfill(9)
+    while retrieve.check(ano) == False:
+        print('Too bad!')
+        ano = input("Please enter a valid arxiv identifier.")
+        ano = ano.zfill(9)
     else: 
-        article = retrieve.arxiv(aX_num)
+        article = retrieve.arxiv(ano)
         print(article)
 
+#a_no = input('Give me a valid arxiv identifier.')
+#cli(a_no)
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 setup(
     name = 'arxiv-script',
     version = '0.1',
-    py_modules = ['arxiv-script', 'article', 'retrieve', 'dir_control'],
+    py_modules = ['arxiv_script', 'article', 'retrieve', 'dir_control'],
     install_requires = ['Click', 
     ],
     entry_points='''
         [console_scripts]
-        arxiv-script = arxiv-script:cli
+        arxiv_script = arxiv_script:cli
     ''',
 )
