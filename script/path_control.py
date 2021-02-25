f2d9d794f76ef166f41fe2f0749d5089680b9a70
flrn <flrn.beck@gmail.com>
2021-02-25 20:57:00 +0100

    Deleted load, check_saved_path etc. in favour of environment variables.

--- script/path_control.py
+++ script/path_control.py
@@ -1,45 +1,18 @@
-""" Some helper functions to load and change path to
-    directory where articles are downloaded to and
-    to the bib-file. Another helper function for
-    opening a downloaded file.
+""" Helper function to check if path exists
+    as directory or file. Another helper function
+    for opening a downloaded file.
 """
 
 import os
 import json
 import platform
 
-def load(path):
-    ''' Load a data file as a json object. '''
-    with open(path, mode = 'r') as f:
-        loaded_data = json.load(f)
-    return loaded_data
-
 def check_path(path, path_type):
     ''' Check if path is of the given path_type (dir or file). '''
-    if path_type == "dir":
+    if path_type == "directory":
         return os.path.isdir(path)
     elif path_type == "file":
         return os.path.isfile(path)
-
-def check_saved_path(file, key, path_type):
-    ''' Loads path (= value of the given key in the file) and
-        checks if it exists as the given path type (directory or file).
-    '''
-    path = load(file)[key]
-    return check_path(path, path_type)
-
-def change_path(file, key, new_path, path_type):
-    ''' Reads path from file (as the value of the given key) and change it to new_path.
-        Checks if the path exists and is of the given path type (directory of file) before changing.
-    '''
-    if check_path(new_path, path_type) == True:
-        loaded_data = load(file)
-        loaded_data[key] = new_path
-        with open(file, 'w') as f:
-            json.dump(loaded_data, f)
-        print('Your new {} is {}.'.format(key, os.path.abspath(new_path)))
-    else:
-        print('Not a correct path. Please try again.')
 
 def get_opener():
     """ To view a pdf-file, we use subprocess.call(opener, file_path).
